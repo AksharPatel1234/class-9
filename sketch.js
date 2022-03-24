f9fda3e532c0d8ca386bf2f325c257b6c3944cb0
AksharPatel1234 <102331801+AksharPatel1234@users.noreply.github.com>
2022-03-24 18:51:51 -0400

    Add files via upload
    I made a sprite move and press space for yellow background.

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var box;
+var edges;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,50,50);
+edges = createEdgeSprites()
+
+
+}
+
+
+
+
+function draw() 
+{
+  background(30);
+if (keyIsDown(LEFT_ARROW)) {
+  box.x = box.x - 5
+}
+if (keyIsDown(RIGHT_ARROW)) {
+  box.x = box.x +5
+}
+if (keyIsDown(DOWN_ARROW)) {
+  box.y = box.y + 5
+}
+if (keyIsDown(UP_ARROW)) {
+  box.y = box.y - 5
+}
+if (keyDown("space")) {
+  background("yellow")
+}
+
+if(box.bounceOff(edges)){
+box.x = 200;
+box.y = 200;
+}
+
+drawSprites();
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
